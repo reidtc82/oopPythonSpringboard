690b2402bb616f5116ebf5006e61fded095bc6fe
reidtc82 <reidtc82@gmail.com>
2021-04-18 23:15:51 -0700

    wrod finder part 2

--- wordfinder.py
+++ wordfinder.py
@@ -9,7 +9,7 @@ class WordFinder:
         self.word_list = self.__get_words(self.f)
 
     def random(self) -> str:
-        return self.word_list[random.randint(0, len(self.word_list))]
+        return self.word_list[random.randint(0, len(self.word_list)-1)]
 
     def __get_words(self, f) -> [str]:
         result_list = []
@@ -20,6 +20,23 @@ class WordFinder:
         return result_list
 
 
+class SpecialWordFinder(WordFinder):
+    def __init__(self, file_location: str):
+        self.f = open(file_location, "r")
+        self.word_list = self.__get_words(self.f)
+
+    def __get_words(self, f):
+        result_list = []
+        for l in f:
+            l = l.strip('\n')
+            if l and '#' not in l:
+                result_list.append(l)
+        
+        # print("{0} words read".format(len(result_list)))
+        return result_list
+
+
+
 if __name__ == "__main__":
-    gen = WordFinder(os.path.join(os.getcwd(), "words.txt"))
+    gen = SpecialWordFinder(os.path.join(os.getcwd(), "alt_words.txt"))
     print(gen.random())
